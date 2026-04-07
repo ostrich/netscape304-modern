07f12406cecf2f28d6266d2b665f8d0f888db88b
ostrich <570911+ostrich@users.noreply.github.com>
2026-04-07 00:08:44 -0400

    Tighten Netscape shim resolver compatibility

--- shim/netscape_compat_shim.c
+++ shim/netscape_compat_shim.c
@@ -2,6 +2,7 @@
 
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <resolv.h>
 #include <sys/socket.h>
 #include <sys/wait.h>
 #include <stdarg.h>
@@ -9,12 +10,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+
+#ifdef _res
+#undef _res
+#endif
 
 /* Netscape copy-relocates these old libc globals with fixed sizes. */
-unsigned char _res[0x180];
+unsigned char _res[0x180] __attribute__((aligned(16)));
 const char *_sys_errlist[124];
 int _sys_nerr = 124;
+
+static char shim_sys_err_storage[124][128];
 
 static int shim_debug_enabled(void) {
   static int initialized = 0;
@@ -39,6 +45,22 @@ static void shim_log(const char *fmt, ...) {
   va_end(ap);
   fputc('\n', stderr);
   fflush(stderr);
+}
+
+__attribute__((constructor))
+static void shim_init_compat_globals(void) {
+  int i;
+
+  memset(_res, 0, sizeof(_res));
+  for (i = 0; i < _sys_nerr; ++i) {
+    const char *msg = strerror(i);
+
+    if (!msg) {
+      msg = "Unknown error";
+    }
+    snprintf(shim_sys_err_storage[i], sizeof(shim_sys_err_storage[i]), "%s", msg);
+    _sys_errlist[i] = shim_sys_err_storage[i];
+  }
 }
 
 /* The binary calls __libc_init(argc, argv, envp) very early. */
@@ -100,46 +122,38 @@ static struct hostent *build_hostent_from_ipv4(const char *name, uint32_t addr_b
   return &shim_hostent;
 }
 
-struct hostent *gethostbyname(const char *name) {
-  char line[512];
-  char ip[INET_ADDRSTRLEN];
-  struct in_addr addr;
+static int run_getent_first_token(const char *database,
+                                  const char *key,
+                                  char *token,
+                                  size_t token_size) {
   int pipefd[2];
   pid_t pid;
-  ssize_t nread;
-  int status;
   FILE *fp;
+  int status;
+  char line[512];
   char *argv[] = {
     "/usr/bin/getent",
-    "ahostsv4",
-    (char *)name,
+    (char *)database,
+    (char *)key,
     NULL
   };
 
-  shim_log("gethostbyname(%s)", name ? name : "(null)");
-  if (!name || !*name) {
-    h_errno = HOST_NOT_FOUND;
-    return NULL;
-  }
-
   if (pipe(pipefd) != 0) {
-    h_errno = TRY_AGAIN;
-    return NULL;
+    return -1;
   }
 
   pid = fork();
   if (pid < 0) {
     close(pipefd[0]);
     close(pipefd[1]);
-    h_errno = TRY_AGAIN;
-    return NULL;
+    return -1;
   }
 
   if (pid == 0) {
     close(pipefd[0]);
     dup2(pipefd[1], STDOUT_FILENO);
-    dup2(pipefd[1], STDERR_FILENO);
     close(pipefd[1]);
+    close(STDERR_FILENO);
     execv(argv[0], argv);
     _exit(127);
   }
@@ -149,40 +163,44 @@ struct hostent *gethostbyname(const char *name) {
   if (!fp) {
     close(pipefd[0]);
     waitpid(pid, NULL, 0);
-    h_errno = TRY_AGAIN;
-    return NULL;
+    return -1;
   }
 
   line[0] = '\0';
   if (!fgets(line, sizeof(line), fp)) {
     fclose(fp);
     waitpid(pid, &status, 0);
-    h_errno = HOST_NOT_FOUND;
-    return NULL;
-  }
-
-  nread = (ssize_t)strlen(line);
-  if (nread > 0 && line[nread - 1] != '\n') {
-    int ch;
-    while ((ch = fgetc(fp)) != EOF && ch != '\n') {
-    }
+    return -1;
   }
 
   fclose(fp);
   if (waitpid(pid, &status, 0) < 0) {
-    h_errno = TRY_AGAIN;
-    return NULL;
+    return -1;
   }
-
   if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
-    h_errno = HOST_NOT_FOUND;
-    return NULL;
+    return -1;
   }
-  if (sscanf(line, "%15s", ip) != 1) {
+  if (sscanf(line, "%255s", token) != 1) {
+    return -1;
+  }
+  token[token_size - 1] = '\0';
+  return 0;
+}
+
+struct hostent *gethostbyname(const char *name) {
+  char ip[INET_ADDRSTRLEN];
+  struct in_addr addr;
+
+  shim_log("gethostbyname(%s)", name ? name : "(null)");
+  if (!name || !*name) {
     h_errno = HOST_NOT_FOUND;
     return NULL;
   }
 
+  if (run_getent_first_token("ahostsv4", name, ip, sizeof(ip)) != 0) {
+    h_errno = HOST_NOT_FOUND;
+    return NULL;
+  }
   if (inet_aton(ip, &addr) == 0) {
     h_errno = HOST_NOT_FOUND;
     return NULL;
@@ -194,6 +212,7 @@ struct hostent *gethostbyname(const char *name) {
 
 struct hostent *gethostbyaddr(const void *addr, socklen_t len, int type) {
   char ip[INET_ADDRSTRLEN];
+  char host[256];
 
   if (!addr || type != AF_INET || len < (socklen_t)sizeof(struct in_addr)) {
     h_errno = HOST_NOT_FOUND;
@@ -204,7 +223,11 @@ struct hostent *gethostbyaddr(const void *addr, socklen_t len, int type) {
     h_errno = HOST_NOT_FOUND;
     return NULL;
   }
+  if (run_getent_first_token("hosts", ip, host, sizeof(host)) != 0) {
+    h_errno = HOST_NOT_FOUND;
+    return NULL;
+  }
 
-  shim_log("reverse lookup fallback %s", ip);
-  return build_hostent_from_ipv4(ip, *(const uint32_t *)addr);
+  shim_log("reverse lookup resolved %s -> %s", ip, host);
+  return build_hostent_from_ipv4(host, *(const uint32_t *)addr);
 }
